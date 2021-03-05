8b5c4ad62aab8ac170bcd234a015d5ca0604020d
stephanh-va <stherzog@deloitte.de>
2021-03-05 11:42:57 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,54 @@
-import { Component } from '@angular/core';
-import * as XLSX from 'xlsx';
+import { Component } from "@angular/core";
+import * as XLSX from "xlsx";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  name = 'This is XLSX TO JSON CONVERTER';
+  name = "This is XLSX TO JSON CONVERTER";
   willDownload = false;
 
-  constructor() { }
+  constructor() {
+  }
+
 
   onFileChange(ev) {
     let workBook = null;
     let jsonData = null;
     const reader = new FileReader();
     const file = ev.target.files[0];
-    reader.onload = (event) => {
+    reader.onload = event => {
       const data = reader.result;
-      workBook = XLSX.read(data, { type: 'binary' });
+      workBook = XLSX.read(data, { type: "binary" });
       jsonData = workBook.SheetNames.reduce((initial, name) => {
         const sheet = workBook.Sheets[name];
-        initial[name] = XLSX.utils.sheet_to_json(sheet);
+        initial[name] = XLSX.utils.sheet_to_json(sheet, {
+          raw: true
+        });
         return initial;
       }, {});
+      console.log(jsonData);
       const dataString = JSON.stringify(jsonData);
-      document.getElementById('output').innerHTML = dataString.slice(0, 300).concat("...");
+      document.getElementById("output").innerHTML = dataString
+        .slice(0, 300)
+        .concat("...");
       this.setDownload(dataString);
-    }
+    };
     reader.readAsBinaryString(file);
   }
-
 
   setDownload(data) {
     this.willDownload = true;
     setTimeout(() => {
       const el = document.querySelector("#download");
-      el.setAttribute("href", `data:text/json;charset=utf-8,${encodeURIComponent(data)}`);
-      el.setAttribute("download", 'xlsxtojson.json');
-    }, 1000)
+      el.setAttribute(
+        "href",
+        `data:text/json;charset=utf-8,${encodeURIComponent(data)}`
+      );
+      el.setAttribute("download", "xlsxtojson.json");
+    }, 1000);
   }
-
-
-
 
 }
